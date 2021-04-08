beeb104ca9b2df421a801d7c37d1ca2d49b856d6
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 21:42:47 +0900

    paper

--- src/main/kotlin/com/github/birang/command/plugin/Main.kt
+++ src/main/kotlin/com/github/birang/command/plugin/Main.kt
@@ -1,11 +1,7 @@
 package com.github.birang.command.plugin
 
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
-import org.bukkit.command.ConsoleCommandSender
-import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin(), Listener {
