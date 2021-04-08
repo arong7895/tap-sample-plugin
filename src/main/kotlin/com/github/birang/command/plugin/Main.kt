80fa3a49e1cb9bd3de19fbc4161ef10f796914d3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 23:49:25 +0900

    paper

--- src/main/kotlin/com/github/birang/command/plugin/Main.kt
+++ src/main/kotlin/com/github/birang/command/plugin/Main.kt
@@ -1,6 +1,7 @@
 package com.github.birang.command.plugin
 
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -8,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin(), Listener {
     val consol = Bukkit.getConsoleSender()
+    var count = 1
 
     override fun onEnable() {
         consol.sendMessage("[Pulgin Activity]")
@@ -19,7 +21,8 @@ class Main : JavaPlugin(), Listener {
     }
 
     @EventHandler
-    fun join(e : PlayerJoinEvent) {
-        e.setJoinMessage("Hello")
+    fun onJoin(e : PlayerJoinEvent) {
+        e.setJoinMessage("Hello " + count)
+        count += 1
     }
 }
