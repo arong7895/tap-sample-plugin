ad2b21b3366858158cc7bba2570d2788f340082e
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 00:59:07 +0900

    paper

--- src/main/kotlin/com/github/birang/command/plugin/Main.kt
+++ src/main/kotlin/com/github/birang/command/plugin/Main.kt
@@ -4,25 +4,42 @@ import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class Main : JavaPlugin(), Listener {
     val consol = Bukkit.getConsoleSender()
     var count = 1
 
     override fun onEnable() {
-        consol.sendMessage("[Pulgin Activity]")
+        consol.sendMessage("${ChatColor.AQUA} [Pulgin Activity]")
         getServer().getPluginManager().registerEvents(this, this)
+
+        saveConfig()
+        val cfile = File(getDataFolder(), "config.yml")
+        if (cfile.length().equals(0)) {
+            getConfig().options().copyDefaults(true)
+            saveConfig()
+        }
     }
 
     override fun onDisable() {
-        consol.sendMessage("[Plugin Disactivity]")
+        consol.sendMessage("${ChatColor.RED} [Plugin Disactivity]")
     }
 
     @EventHandler
-    fun onJoin(e : PlayerJoinEvent) {
-        e.setJoinMessage("Hello " + count)
-        count += 1
+    fun onJoin(e: PlayerJoinEvent) {
+        val msg = this.getConfig().getString("접속메시지")
+        e.setJoinMessage("${ChatColor.GOLD} $msg")
+    }
+
+    @EventHandler
+    fun breakBlock(e: BlockBreakEvent) {
+        val msg1 = this.getConfig().getString("블록메시지.첫번째")
+        val msg2 = this.getConfig().getString("블록메시지.세번째")
+        e.getPlayer().sendMessage(msg1!!)
+        e.getPlayer().sendMessage(msg2!!)
     }
 }
